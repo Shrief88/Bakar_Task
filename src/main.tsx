b1798e7a90d9aa3fb083bf914ee6379da05ea155
Shrief Essam <shriefessam1999@gmail.com>
2025-01-17 20:15:02 +0200

    Add react table

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import routes from "./routes.tsx";
 
 const router = createBrowserRouter(routes);
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
