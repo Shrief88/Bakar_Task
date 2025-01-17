493080ca1bef8d91ac6a529959ade5150efeddde
Shrief Essam <shriefessam1999@gmail.com>
2025-01-17 18:57:32 +0200

    Add tanstack table

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,7 +1,31 @@
+import { DataTable } from "@/components/DataTable";
+import { columns, ColumnProps } from "@/interfaces/column";
+import data from "@/data/data.json";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const Transactions = () => {
   return (
-    <div>Transactions</div>
-  )
-}
+    <div className="container mx-auto py-10">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Welcome back!</CardTitle>
+          <CardDescription className="text-md">Here are your recent transactions</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={data.transactions as ColumnProps[]}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default Transactions
+export default Transactions;
